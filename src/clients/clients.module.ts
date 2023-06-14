071ad3e1f0853894c88e7035ebde321e8dc418ed
SebastianSanchez22 <sebastiansaor22@gmail.com>
2023-06-14 02:02:02 -0500

    Overriding dependencies and upsating clients module

--- src/clients/clients.module.ts
+++ src/clients/clients.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ClientsService } from './clients.service';
 import { ClientsController } from './clients.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Client, ClientSchema } from './entities/client.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Client.schema_name,
+        schema: ClientSchema
+      }
+    ])],
   controllers: [ClientsController],
   providers: [ClientsService]
 })
